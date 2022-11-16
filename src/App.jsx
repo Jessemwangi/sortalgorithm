eb092f72032c87202cea7fe47faad4f66750410b
Jessemwangi <jessejzee.com>
2022-11-16 08:33:56 +0200

    selection sorting start., sorrting by selecting the lowest

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state ={
+    
+    unsortedArray:[],
+    inputValue:'',
+    sortedArray:'',
+  }
+  ReadArray = (e) =>{
+    e.preventDefault();
+    this.setState({
+      [e.target.name]:e.target.value.split(','),
+    })
+    console.log(this.state.unsortedArray);
+  }
+
+  StartSorting = (e) =>{
+    for (let i = 0; i < this.state.unsortedArray.length; i++) {
+      let lowest = i
+      for (let j = i + 1; j < this.state.unsortedArray.length; j++) {
+        if (this.state.unsortedArray[j] < this.state.unsortedArray[lowest]) {
+          lowest = j
+        }
+      }
+      if (lowest !== i) {
+        // Swap
+        ;[this.state.unsortedArray[i], this.state.unsortedArray[lowest]] = [this.state.unsortedArray[lowest], this.state.unsortedArray[i]]
+      }
+    }
+    
+    e.preventDefault();
+    this.setState({
+      sortedArray:'sorted hurray' + this.state.unsortedArray
+    });
+    console.log(this.state.unsortedArray);
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1> enter value separated by commer to sort</h1>
+        <form action="">
+      <div className='main'>
+        <label htmlFor="values">enter values</label>
+        <input type="text" name="unsortedArray" id="values" onChange={this.ReadArray} />
+      </div>
+      <button type='submit' onClick={this.StartSorting}> sort data</button>
+      <div>
+        <p>{this.state.unsortedArray}</p>
+      </div>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
---
+++ src/sort.md
@@ -0,0 +1,15 @@
+## selection sort
+
+select the first element as the smallent then compare it, and when we find the small we swap it with that
+
+
+<ol>
+<li>First, we create an outer loop to go over each element once, and store the first element as the smallest value we’ve seen so far in a variable called “lowest”.
+</li>
+<li>
+We then create an inner for-loop to go over each element in the array, starting from i + 1.</li>
+<li>
+Inside this inner-loop, we check if we the current value is less than our current lowest. If so, we update our lowest to the index of that value.</li
+<li>
+After the inner-loop has reached the end of the array, we check to see if the lowest ever got changed; if so, we swap it with arr[i].</li><li>
+We then go back to outer loop, increment i by 1 (element 2) and repeat the process until each element in the array has been checked.</li>
